d99433633f3ab0d17eb8f765759f790c7c6f6416
ASHIKA16467 <169030231+ASHIKA16467@users.noreply.github.com>
2025-08-02 12:09:48 +0530

    Delete backend directory

--- backend/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy(); // Destroy all sessions
-header("Location: ../frontend/login.html"); // Redirect to login page
-exit();
-?>
--- backend/test_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'config.php';
-
-if ($conn) {
-    echo "Database connected successfully!";
-} else {
-    echo "Database connection failed.";
-}
-?>
