5a468e704afa7074f4fe1a35347c883a8bae8662
ASHIKA16467 <169030231+ASHIKA16467@users.noreply.github.com>
2025-08-02 12:10:01 +0530

    Delete database directory

--- database/creadential.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE user_credentials (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    platform VARCHAR(50) NOT NULL,
-    username VARCHAR(255) NOT NULL,
-    password VARBINARY(255) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
